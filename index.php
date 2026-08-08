cb260f218a3f6cd29b84831817274fa0b8e664df
Bigyasha Maharjan <maharjanbigyasha@gmail.com>
2026-08-08 11:08:37 +0545

    Add hero section

--- index.php
+++ index.php
@@ -4,17 +4,21 @@
         <title>TravelGo</title>
 </head>
 <body>
-        <h1>Welcome to TravelGo</h1>
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="About.php">About</a></li>
-                <li><a href="Packages.php">Packages</a></li>
-                <li><a href="Destinations.php">Destinations</a></li>
-                <li><a href="Contact.php">Contact</a></li>
-                <li><a href="Login/Signup.php">Login/Signup</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="packages.php">Packages</a></li>
+                <li><a href="destinations.php">Destinations</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="login/signup.php">Login/Signup</a></li>
             </ul>
         </nav>
+        <section class="hero">
+            <h1>Explore Nepal with TravelGo</h1>
+            <p>Discover the beauty of Nepal with our exclusive travel packages.</p>
+            <a  href="packages.php" class="btn">View Packages</a>
+        </section>
 </body>
 
 </html>
