30395b3ad575bbbf8bcc7ab9b2bfdf51f47e32e5
Bigyasha Maharjan <maharjanbigyasha@gmail.com>
2026-08-08 11:03:41 +0545

    Add nav bar

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
+<!DOCTYPE html>
 <html>
 <head>
         <title>TravelGo</title>
 </head>
 <body>
         <h1>Welcome to TravelGo</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="About.php">About</a></li>
+                <li><a href="Packages.php">Packages</a></li>
+                <li><a href="Destinations.php">Destinations</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+                <li><a href="Login/Signup.php">Login/Signup</a></li>
+            </ul>
+        </nav>
 </body>
 
 </html>
